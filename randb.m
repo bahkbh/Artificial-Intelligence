49b9a8063ab29a952c17974ec916b6d8424654c1
bahkbh <bahkbh@gmail.com>
2024-11-24 13:05:51 +0900

    인공지능

--- randb.m
+++
@@ -1,9 +0,0 @@
-function output = randb(dimension), 
-% Binary random variable
-% This function returns only a vector. 
-
-y = rand(dimension,1); 
-output(find(y>=0.5))=1; 
-output(find(y<0.5))=0; 
-
-
